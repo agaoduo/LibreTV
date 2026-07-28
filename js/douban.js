a42b5d51defde7a8f3c19dcc9bf7a794cb4d23f9
agaoduo <152761895+agaoduo@users.noreply.github.com>
2026-07-28 12:11:23 +0800

    Update proxiedCoverUrl to use new proxy service

--- js/douban.js
+++ js/douban.js
@@ -496,7 +496,8 @@ function renderDoubanCards(data, container) {
             const originalCoverUrl = item.cover;
             
             // 2. 也准备代理URL作为备选
-            const proxiedCoverUrl = PROXY_URL + encodeURIComponent(originalCoverUrl);
+            const proxiedCoverUrl = "https://tiny-art-543c.wenxiang7553.workers.dev/?url=" + encodeURIComponent(originalCoverUrl);
+
             
             // 为不同设备优化卡片布局
             card.innerHTML = `
